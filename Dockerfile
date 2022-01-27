57582eee266681c54405d7636352811d4640bed5
taiichimotoi <tm050142@hotmail.co.jp>
2022-01-27 14:15:37 +0900

    Fixed some bugs in container_start.sh

--- Dockerfile
+++ Dockerfile
@@ -9,4 +9,10 @@ RUN apk add --no-cache expect
 #👇GitHub CLIを追加
 RUN apk add --no-cache github-cli
 
+ENV PS1="[\u@\h:\w]$"
+
+RUN addgroup -g 1000 -S gituser && \
+    adduser -D -u 1000 -S -G gituser gituser
+RUN echo '%gituser ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
+
 CMD ["bash"]
--- container_start.sh
+++ container_start.sh
@@ -7,7 +7,9 @@ docker cp ~/.ssh/id_ed25519 my-app-dev:/usr/src/app/.ssh/
 docker-compose exec app bash -c '
     eval "$(ssh-agent)"
     ssh-add .ssh/id_ed25519
-    ssh -T git@github.com
-    expect -c "expect yes {send yes}"
+    expect -c "
+        spawn ssh -T git@github.com
+        expect \"Are you sure you want to continue\" { send yes }
+    "
     bash
 '
