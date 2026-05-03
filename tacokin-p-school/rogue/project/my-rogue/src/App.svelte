75266f484d8f1c86e25d407fad775c5cc12d2a54
taiichimotoi <tm050142@hotmail.co.jp>
2026-05-03 17:14:52 +0900

    Fixed bug

--- tacokin-p-school/rogue/project/my-rogue/src/App.svelte
+++ tacokin-p-school/rogue/project/my-rogue/src/App.svelte
@@ -6,14 +6,42 @@
     const cols = 18;//横ブロック数
     const rows = 18;//縦ブロック数
 
-    let mapStrings = [];
+    //マップ配列
+    const mapArray = new Array(cols);
+    for (let i = 0; i < cols; i++) {
+        mapArray[i] = new Array(rows);
+    }
+
     const dungeon = new ROT.Map.Digger(cols, rows);
-    dungeon.create((x, y, value) => {
-        if (!mapStrings[y]) mapStrings[y] = "";
-        // value 0=床(.), 1=壁(#)
-        mapStrings[y] += (value === 0 ? "." : "#");
+    //マップ配列に情報を書き込む
+    dungeon.create((x, y, type) => {
+        // type: 0=床(.), 1=壁(#)
+        mapArray[x][y] = type;
     });
-    console.log('[Info]dungeon:', dungeon);
+
+    //床(0)に隣接した壁を2へ変更
+    const checkWall = (map) => {
+        for (let i=1; i< map.length-1; i++) {
+            for (let j=1; j< map[i].length-1; j++) {
+                if (map[i][j] == 0) {
+                    if (map[i-1][j] == 1) map[i-1][j] = 2;
+                    if (map[i-1][j-1] == 1) map[i-1][j-1] = 2;
+                    if (map[i+1][j] == 1) map[i+1][j] = 2;
+                    if (map[i-1][j+1] == 1) map[i-1][j+1] = 2;
+                    if (map[i][j-1] == 1) map[i][j-1] = 2;
+                    if (map[i+1][j-1] == 1) map[i+1][j-1] = 2;
+                    if (map[i][j+1] == 1) map[i][j+1] = 2;
+                    if (map[i+1][j+1] == 1) map[i+1][j+1] = 2;
+                }
+            }
+        }
+    };
+    checkWall(mapArray);
+
+    // マップデータ（文字列配列）へ変更
+    const mapStrings = mapArray.map(row => row.map(cell => {
+        return cell == 0 ? "." : cell == 1 ? "#" : cell == 2 ? "=" : "";
+    }).join(""));
 
     const k = kaplay({
         width: 32*cols,
@@ -35,10 +63,14 @@
         tiles: {
             ".": () => [
                 k.sprite("floor", {width: 32, height: 32}),
-                // k.area(),
                 "floor",
             ],
             "#": () => [
+                k.rect(32, 32),
+                k.color(25, 25, 25),
+                "bulk",
+            ],
+            "=": () => [
                 k.sprite("wall", {width: 32, height: 32}),
                 // k.area(),
                 // k.body({ isStatic: true }), // 壁は動かない＆ぶつかる
@@ -54,28 +86,55 @@
         k.sprite("player", {width: 32, height: 32}),
         k.pos(startPos[0] * TILE_SIZE, startPos[1] * TILE_SIZE),
         k.area(),
-        // k.body(),
+        k.body(),
         "player",
     ]);
 
+    const checkForward = (x, y) => {
+        const objects = level.get("wall");
+        const targetObj = objects.find(obj => {
+            return obj.pos.x == x && obj.pos.y == y
+        });
+
+        if (targetObj) {
+            // オブジェクトが見つかった場合true
+            return true;
+        }
+        return false;
+    }
+
+    let lock = false;
+
     k.onKeyPress("left", () => {
+        const anchor = player.pos.add(-TILE_SIZE, 0);
+        if (checkForward(anchor.x, anchor.y)) return;
         player.pos.x -= TILE_SIZE;
-        // player.move(-600, 0);
+        // lock = true;
+        // k.tween(player.pos, anchor, 0.2, (p) => {
+        //     player.pos = p;
+        //     if (p == anchor) lock = false;
+        // }, k.easings.linear);
     });
 
     k.onKeyPress("right", () => {
+        const anchor = player.pos.add(TILE_SIZE, 0);
+        if (checkForward(anchor.x, anchor.y)) return;
         player.pos.x += TILE_SIZE;
-        // player.move(600, 0);
+        // k.tween(player.pos, anchor, 0.2, (p) => player.pos = p, k.easings.linear);
     });
 
     k.onKeyPress("up", () => {
+        const anchor = player.pos.add(0, -TILE_SIZE);
+        if (checkForward(anchor.x, anchor.y)) return;
         player.pos.y -= TILE_SIZE;
-        // player.move(0, -600);
+        // k.tween(player.pos, anchor, 0.2, (p) => player.pos = p, k.easings.linear);
     });
 
     k.onKeyPress("down", () => {
+        const anchor = player.pos.add(0, TILE_SIZE);
+        if (checkForward(anchor.x, anchor.y)) return;
         player.pos.y += TILE_SIZE;
-        // player.move(0, 600);
+        // k.tween(player.pos, anchor, 0.2, (p) => player.pos = p, k.easings.linear);
     });
 
 </script>
